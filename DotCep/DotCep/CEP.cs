3d4b42b80249bc7dba2b1738b088875de13da96e
Cristiano Cunha <cristianoraffi@bol.com.br>
2016-10-21 16:07:39 -0200

    Informacoes do endereço pelo CEP.

---
+++ DotCep/DotCep/CEP.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DotCep
+{
+    public static class CEP
+    {
+        public static Endereco ObterEndereco(uint CEP)
+        {
+            Endereco enderecoBase = new Endereco();
+
+            if (VerificarValidadeDoCep(CEP))
+            {
+                String StrJSON = ControleJSON.ObterStringJSONS(CEP);
+
+                if (VerificarExistencia(StrJSON))
+                {
+                    enderecoBase = JsonConvert.DeserializeObject<Endereco>(StrJSON);
+                }
+            }
+
+            return enderecoBase;
+        }
+
+        /// <summary>
+        /// Verifica se o CEP (válido) existe, ou são apenas 8 numeros que não formam um CEP verdadeiro.
+        /// </summary>
+        /// <returns><c>true</c>, if existencia was verificared, <c>false</c> otherwise.</returns>
+        /// <param name="JSON">JSO.</param>
+        private static bool VerificarExistencia(string JSON)
+        {
+            if (JSON.Contains("\"erro\": true"))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Verifica se o CEP é válido, se ele contém 8 numeros.
+        /// </summary>
+        /// <returns><c>true</c>, if validade do cep was verificared, <c>false</c> otherwise.</returns>
+        /// <param name="CEP">CE.</param>
+        private static bool VerificarValidadeDoCep(uint CEP)
+        {
+            if (CEP.ToString().Length == 8)
+            {
+                return true;
+            }
+            else
+            {    
+                return false;
+            }
+        }
+
+    }
+}
+
---
+++ DotCep/DotCep/ControleJSON.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace DotCep
+{
+    internal static class ControleJSON
+    {
+        internal static string ObterStringJSONS(uint CEP)
+        {
+            HttpWebRequest request =
+                (HttpWebRequest)WebRequest.Create(ObterURL(CEP));
+            
+            WebResponse response = request.GetResponse();
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(
+                                          stream, Encoding.UTF8);
+                return reader.ReadToEnd();
+            }
+        }
+
+        private static string ObterURL(uint CEP)
+        {
+            const String CaminhoPadrao = @"https://viacep.com.br/ws/{0}/json/";
+            return String.Format(CaminhoPadrao, CEP.ToString());
+        }
+    }
+}
+

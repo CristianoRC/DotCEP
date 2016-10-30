54f5dfcfe7e2286835129d23a78d22e6d082fb89
Cristiano Cunha <cristianoraffi@bol.com.br>
2016-10-30 12:03:08 -0200

    Versão

--- DotCep/DotCep/Properties/AssemblyInfo.cs
+++ DotCep/DotCep/Properties/AssemblyInfo.cs
@@ -9,13 +9,13 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("cristiano")]
+[assembly: AssemblyCopyright ("Cristiano Raffi Cunha")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.4")]
+[assembly: AssemblyVersion ("1.1.1")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]

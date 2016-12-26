c96ac09bd855ed1c6755deb4775aab7b105d215c
Cristiano Cunha <cristianoraffi@bol.com.br>
2016-12-26 13:33:13 -0200

    Metodos de manipulacao das informacoes dos municipios brasileiros

--- DotCep/DotCep/Localidades/Municipio.cs
+++ DotCep/DotCep/Localidades/Municipio.cs
@@ -12,11 +12,132 @@ namespace DotCEP.Localidades
 		public string Nome { get; set; }
 		#endregion
 
+		public static Municipio ObterInformacoesDoMunicipio(string NomeMunicipio)
+		{
+			Municipio municipioBase = new Municipio();
+			Spartacus.Database.Command cmd = new Spartacus.Database.Command();
+			DataTable tabelaResultado = new DataTable();
+
+			cmd.v_text = "Select * from Municipios m where m.nome = #nome#";
+			cmd.AddParameter("nome", Spartacus.Database.Type.STRING);
+			cmd.SetValue("nome", NomeMunicipio);
+
+			tabelaResultado = ObterInformacoesDoBanco(cmd.GetUpdatedText());
+
+			if (tabelaResultado.Rows.Count != 0)
+			{
+				municipioBase = ConverterRowParaEntidade(tabelaResultado.Rows[0]);
+			}
+
+
+			return municipioBase;
+		}
+
+		public static Municipio ObterInformacoesDoMunicipio(uint CodigoMunicipio)
+		{
+			Municipio municipioBase = new Municipio();
+			Spartacus.Database.Command cmd = new Spartacus.Database.Command();
+			DataTable tabelaResultado = new DataTable();
+
+			cmd.v_text = "Select * from Municipios m where m.Codigo = #codigo#";
+			cmd.AddParameter("codigo", Spartacus.Database.Type.INTEGER);
+			cmd.SetValue("codigo", CodigoMunicipio.ToString());
+
+			tabelaResultado = ObterInformacoesDoBanco(cmd.GetUpdatedText());
+
+			if (tabelaResultado.Rows.Count != 0)
+			{
+				municipioBase = ConverterRowParaEntidade(tabelaResultado.Rows[0]);
+			}
+
+			return municipioBase;
+		}
+
+		public static String ObterNomeDoMunicipio(uint CodigoMunicipio)
+		{
+			String saida = String.Empty;
+			Spartacus.Database.Command cmd = new Spartacus.Database.Command();
+			DataTable tabelaResultado;
+
+			cmd.v_text = "select t.nome from Municipios t where t.codigo = #codigo#";
+			cmd.AddParameter("codigo", Spartacus.Database.Type.INTEGER);
+			cmd.SetValue("codigo", CodigoMunicipio.ToString());
+
+			tabelaResultado = ObterInformacoesDoBanco(cmd.GetUpdatedText());
+
+			if (tabelaResultado.Rows.Count != 0)
+			{
+				saida = tabelaResultado.Rows[0]["nome"].ToString();
+			}
+
+			return saida;
+		}
+
+		public static int ObterCodigoDoMunicipio(String NomeMunicipio, ushort CodigoEstado)
+		{
+			int saida = 0;
+			Spartacus.Database.Command cmd = new Spartacus.Database.Command();
+			DataTable tabelaResultado;
+
+			cmd.v_text = "select t.Codigo from Municipios t where t.nome = #nome# and t.CodigoEstado = #estado#";
+			cmd.AddParameter("nome", Spartacus.Database.Type.STRING);
+			cmd.AddParameter("estado", Spartacus.Database.Type.INTEGER);
+
+			cmd.SetValue("nome", NomeMunicipio);
+			cmd.SetValue("estado", CodigoEstado.ToString());
+
+			tabelaResultado = ObterInformacoesDoBanco(cmd.GetUpdatedText());
+
+			if (tabelaResultado.Rows.Count != 0)
+			{
+				saida = Convert.ToInt32(tabelaResultado.Rows[0]["Codigo"].ToString());
+			}
+
+			return saida;
+		}
+
+		public static List<Municipio> ObterListaDeMunicipio()
+		{
+			List<Municipio> listaDeMunicipios = new List<Municipio>();
+			DataTable tabelaResultado;
+			Spartacus.Database.Command cmd = new Spartacus.Database.Command();
+
+			cmd.v_text = "select t.* from Municipios t";
+			tabelaResultado = ObterInformacoesDoBanco(cmd.GetUpdatedText());
+
+			foreach (DataRow item in tabelaResultado.Rows)
+			{
+				listaDeMunicipios.Add(ConverterRowParaEntidade(item));
+			}
+
+			return listaDeMunicipios;
+		}
+
+		public static List<Municipio> ObterListaDeMunicipio(ushort CodigoEstado)
+		{
+			List<Municipio> listaDeMunicipios = new List<Municipio>();
+			DataTable tabelaResultado;
+			Spartacus.Database.Command cmd = new Spartacus.Database.Command();
+
+			cmd.v_text = "select t.* from Municipios t where t.CodigoEstado = #codigo#";
+			cmd.AddParameter("codigo", Spartacus.Database.Type.INTEGER);
+			cmd.SetValue("codigo", CodigoEstado.ToString());
+
+			tabelaResultado = ObterInformacoesDoBanco(cmd.GetUpdatedText());
+
+			foreach (DataRow item in tabelaResultado.Rows)
+			{
+				listaDeMunicipios.Add(ConverterRowParaEntidade(item));
+			}
+
+			return listaDeMunicipios;
+		}
+
 		private static Municipio ConverterRowParaEntidade(DataRow LinhaDaTabela)
 		{
 			Municipio municipioBase = new Municipio();
 
-			municipioBase.Codigo = Convert.ToInt16(LinhaDaTabela["Codigo"]);
+			municipioBase.Codigo = Convert.ToInt32(LinhaDaTabela["Codigo"]);
 			municipioBase.CodigoEstado = Convert.ToInt16(LinhaDaTabela["CodigoEstado"]);
 			municipioBase.Nome = LinhaDaTabela["Nome"].ToString();
 

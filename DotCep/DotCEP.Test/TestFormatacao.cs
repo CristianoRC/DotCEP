c350c7897582c21ea1b2c394dd0ac2092801f38f
Cristiano Cunha <cristianoraffi@bol.com.br>
2017-01-04 10:27:18 -0200

    Implementacao de Testes unitarios
    Testes unitarios das classes de validacao e formtacao

---
+++ DotCep/DotCEP.Test/TestFormatacao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DotCEP.Test
+{
+	[TestFixture]
+	public class TestFormatacao
+	{
+		[Test]
+		public void TestFormatacaoComEspacosEmBranco()
+		{
+			String CEPFormatado = DotCEP.Formatacao.FormatarCEP("9 6 0 8 5 0 0 0");
+
+			Assert.AreEqual("96085-000", CEPFormatado);
+		}
+
+		[Test]
+		public void TestCepFormatadoErrado()
+		{
+			String CEPFormatado = DotCEP.Formatacao.FormatarCEP("96-085 000");
+
+			Assert.AreEqual("96085-000", CEPFormatado);
+		}
+
+		[TestCase("960850000")]
+		[TestCase("96085=000")]
+		public void TestNaoValido(String CEPparaVerificar)
+		{
+			String CEPFormatado = DotCEP.Formatacao.FormatarCEP(CEPparaVerificar);
+
+			Assert.AreEqual(String.Empty, CEPFormatado);
+		}
+	}
+}
---
+++ DotCep/DotCEP.Test/TestValidacao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DotCEP.Test
+{
+	[TestFixture]
+	public class TestValidacao
+	{
+		[TestCase("96085-000")]
+		[TestCase("96085000")]
+		public void TestVerificacaoCEPValido(String CepTeste)
+		{
+			Boolean resultadoValidacao = DotCEP.Validacoes.VerificarValidadeDoCep(CepTeste);
+			Assert.AreEqual(true, resultadoValidacao);
+		}
+
+		[TestCase("960850-00")]
+		[TestCase("960850000")]
+		public void TestVerificacaoCEPInvalido(String CepTeste)
+		{
+			bool ResultadoValidacao = DotCEP.Validacoes.VerificarValidadeDoCep(CepTeste);
+			Assert.AreEqual(false, ResultadoValidacao);
+		}
+
+		[Test]
+		public void TestVerificacaoDeCEPExistente()
+		{
+			bool resultadoDaExistencia = DotCEP.Validacoes.VerificarExistenciaDoCEP("96085100");
+			Assert.AreEqual(true, resultadoDaExistencia);
+		}
+
+		[Test]
+		public void TestVerificacaoDeCEPInexistente()
+		{
+			bool resultadoDaExistencia = DotCEP.Validacoes.VerificarExistenciaDoCEP("960850000");
+			Assert.AreEqual(false, resultadoDaExistencia);
+		}
+	}
+}

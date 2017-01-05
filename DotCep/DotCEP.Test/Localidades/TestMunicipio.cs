686d884c158902a64a284865e2cfcd2b52506931
Cristiano Cunha <cristianoraffi@bol.com.br>
2017-01-05 15:10:12 -0200

    Testes unitarios do Municipio

--- DotCep/DotCEP.Test/Localidades/TestMunicipio.cs
+++ DotCep/DotCEP.Test/Localidades/TestMunicipio.cs
@@ -6,5 +6,44 @@ namespace DotCEP.Test
 	[TestFixture]
 	public class TestMunicipio
 	{
+		[Test]
+		public void TestObtendoCodigoDoMunicipio()
+		{
+			int Resultado = DotCEP.Localidades.Municipio.ObterCodigoDoMunicipio("Pelotas", UF.RS);
+
+			Assert.AreEqual(4314407, Resultado);
+		}
+
+		[Test]
+		public void TestObtendoInformacoesDoMunicipio()
+		{
+			Localidades.Municipio informacoesMunicipio = Localidades.Municipio.ObterInformacoesDoMunicipio(4314407);
+
+			Assert.AreEqual(4314407, informacoesMunicipio.Codigo);
+			Assert.AreEqual(43, informacoesMunicipio.CodigoEstado);
+			Assert.AreEqual("Pelotas", informacoesMunicipio.Nome);
+		}
+
+		[Test]
+		public void TestObtendoNomeDoMunicipio()
+		{
+			String Resultado = Localidades.Municipio.ObterNomeDoMunicipio(4314407);
+
+			Assert.AreEqual("Pelotas", Resultado);
+		}
+
+		[Test]
+		public void TestListaDeTodosMunicipios()
+		{
+			int numeroDeTodosRegistros = Localidades.Municipio.ObterListaDeMunicipio().Count;
+			Assert.AreEqual(5570, numeroDeTodosRegistros);
+		}
+
+		[Test]
+		public void TestListaDeMunicipiosPorEstado()
+		{
+			int numeroDeTodosRegistrosDoRS = Localidades.Municipio.ObterListaDeMunicipio(UF.RS).Count;
+			Assert.AreEqual(497, numeroDeTodosRegistrosDoRS);
+		}
 	}
 }

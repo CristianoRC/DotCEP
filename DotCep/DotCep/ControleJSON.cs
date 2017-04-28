9e3d276bdc758ae0858924ea76180d52ffb25890
Cristiano Raffi Cunha <contato@cristianoprogramador.com>
2017-04-28 20:48:57 -0300

    Versão do NUnit

--- DotCep/DotCep/ControleJSON.cs
+++ DotCep/DotCep/ControleJSON.cs
@@ -5,33 +5,32 @@ namespace DotCEP
 	internal static class ControleJSON
 	{
 
-		internal static string ObterCEPdaStrJSON(string strJSON)
+		internal static string ObterCEPdaStrJSON (string strJSON)
 		{
-			Endereco enderecobase = new Endereco();
+			Endereco enderecobase = new Endereco ();
 
-			enderecobase = Newtonsoft.Json.JsonConvert.DeserializeObject<Endereco>(strJSON);
+			enderecobase = Newtonsoft.Json.JsonConvert.DeserializeObject<Endereco> (strJSON);
 
-			string CEPtemp = enderecobase.cep.Replace("-", "");
+			string CEPtemp = enderecobase.cep.Replace ("-", "");
 
-			return CEPtemp;
+			return CEPtemp;	
 		}
 
 		/// <summary>
 		/// Separa o array JSON em objetos e logo após converte novamente para um objeto em JSON.
 		/// </summary>
 		/// <param name="strJSON">String json.</param>
-		internal static List<string> SepararArrayJSON(string strJSON)
+		internal static List<string> SepararArrayJSON (string strJSON)
 		{
-			List<string> EnderecosJSON = new List<string>();
+			List<string> EnderecosJSON = new List<string> ();
 
-			List<Endereco> Enderecos = new List<Endereco>();
+			List<Endereco> Enderecos = new List<Endereco> ();
 
-			Enderecos = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Endereco>>(strJSON);
+			Enderecos = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Endereco>> (strJSON);
 
 
-			foreach (Endereco item in Enderecos)
-			{
-				EnderecosJSON.Add(Newtonsoft.Json.JsonConvert.SerializeObject(item));
+			foreach (Endereco item in Enderecos) {
+				EnderecosJSON.Add (Newtonsoft.Json.JsonConvert.SerializeObject (item));
 			}
 
 			return EnderecosJSON;
